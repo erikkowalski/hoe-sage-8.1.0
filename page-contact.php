7e2d5ac725d17d8d3cd3522573dee03e2e6fc04d
erikkowalski <erikkowalski@mac.com>
2015-05-22 09:39:32 -0400

    Added Contact page styling and Google map Iframe

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
+<section class="contact-info">
 <?php get_template_part('templates/content', 'page'); ?>
+</section>
 <?php endwhile; ?>
