66daaa0e6b7dc35b9d782cdee7ffa38f1da7d28e
erikkowalski <erikkowalski@mac.com>
2015-05-26 15:35:53 -0400

    Added Custom fields into Grid Template

--- template-grid.php
+++ template-grid.php
@@ -4,10 +4,31 @@
  */
 ?>
 
+<img class="img-responsive" src="<?php the_field('grid_header_image') ?>" alt="header image">
+<hr/>
 
 
 <?php while (have_posts()) : the_post(); ?>
+<section>
+<div class="col-sm-6">
+
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page'); ?>
+</div>
+<div class="col-sm-6">
+    <img class="img-responsive" src="<?php the_field('grid_image_one') ?>" alt="store image">
+</div>
+</section>
+<hr/>
+<section>
+    <div class="col-sm-6" >
+        <img class="img-responsive" src="<?php the_field('grid_image_two') ?>" alt="store image">
+    </div >
+    <div class="col-sm-6">
+        <h2><?php the_field('grid_headline_two') ?></h2>
+        <p><?php the_field('grid_paragraph_two') ?></p>
+    </div>
+
+</section>
 
 <?php endwhile; ?>
