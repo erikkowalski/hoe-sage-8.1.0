3677e653d2ca297cdd7c1ca03a05ef7f9062889d
erikkowalski <erikkowalski@mac.com>
2015-06-01 14:34:33 -0400

    Added Copy right and link to edkstuio to site footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,9 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <div class="container">
+     <hr/>
+      <p>How On Earth &copy; <?php echo date_i18n('F Y', time()); ?></p>
+      <p>Website Design by: <a href="http://edkstudio.com">edk studio</a> </p>
+    </div>
 </footer>
